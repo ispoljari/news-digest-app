de2fd38a8b765f7bdcee31eb4402ec8f70954565
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-09-12 14:48:45 +0200

    html, css, js: finnish the home screen transition, block the initial (load-screen) animations, connect the navigation links to pages

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ function startApp() {
   fadeInHamburgerIcon();
   // setFocusOnSelect();
   toggleHamburgerIconListen();
-  detectSrollDown();
+  scrollUpDownListen();
+  navigationLinksListen();
+  blockInitialAnimations();
 }
 
 // page load animations
@@ -45,7 +47,7 @@ function slideDownTheMenuStrip() {
 
 // page transition triger and animations
 
-function detectSrollDown() {
+function scrollUpDownListen() {
   setTimeout(function() {
     $('body').on('mousewheel',function(event) {
       if (event.originalEvent.wheelDelta>0) {
@@ -56,16 +58,29 @@ function detectSrollDown() {
         fadeOutHeading();
         // fadeInSearchForm();
       }
-    })
+    });
   }, 4000);
+}
+
+function navigationLinksListen() {
+  setTimeout(function() {
+    $('body').on('click','.js-home',function() {
+      fadeInHeading();
+      $('.js-home').blur();
+    });
+
+    $('body').on('click','.js-search',function() {
+      fadeOutHeading();
+      $('.js-search').blur();
+    });
+  }, 4000);
+  
 }
 
 function fadeOutHeading() {
   $('.js-headings').addClass('eject-up-scroll fade-up-scroll');
 
   setTimeout(function() {
-    $('.js-headings').css('top','+1000px');
-
     $('.js-h1').removeClass('animate-typing-h1');
     $('.js-h2').removeClass('animate-typing-h2');
     $('.js-headings').css('display','none');
@@ -73,13 +88,29 @@ function fadeOutHeading() {
 }
 
 function fadeInHeading() {
-  $('.js-headings').css({'display':'block','top':''}).removeClass('eject-up-scroll');
-  $('.js-h1').css('opacity','1');
-  $('.js-h2').css('opacity','1');
+  $('.js-headings').css('display','block').removeClass('eject-up-scroll');
 
   setTimeout(function() {
     $('.js-headings').removeClass('fade-up-scroll');
   }, 400);
+}
+
+function blockInitialAnimations() {
+  setTimeout(function() {
+    $('.js-h1').css('opacity','1');
+    $('.js-h2').css('opacity','1');
+
+    $('.js-h1').removeClass('animate-typing-h1');
+    $('.js-h2').removeClass('animate-typing-h2');
+  }, 3600);
+
+  setTimeout(function() {
+    $('.js-large-screen-nav').css('opacity','1');
+
+    $('.js-large-screen-nav').removeClass('large-screen-nav-animate');
+    $('.js-nav-bullet').removeClass('nav-bullet-animate');
+    $('.js-nav-text').removeClass('nav-text-animate');
+  }, 5500);
 }
 
 $(startApp);
